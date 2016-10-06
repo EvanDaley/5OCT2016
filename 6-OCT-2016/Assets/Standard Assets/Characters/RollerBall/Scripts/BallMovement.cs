3d1b5522b6ce33f697b1b1b8773b218a0fb68d11
Evan <daley.evan@gmail.com>
2016-10-05 19:38:28 -0700

    Create basic movement

---
+++ 6-OCT-2016/Assets/Standard Assets/Characters/RollerBall/Scripts/BallMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallMovement : MonoBehaviour {
+
+	private Rigidbody rigidbody;
+	public float moveForce;
+
+	void Start()
+	{
+		rigidbody = GetComponent < Rigidbody>();
+	}
+
+	void Update () {
+		float x = Input.acceleration.y*.5f;
+		float y = -Input.acceleration.x*.5f;
+		rigidbody.AddForce(new Vector3 (x, 0, y) * moveForce);
+	}
+}
